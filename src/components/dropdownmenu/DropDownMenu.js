2ed5a7ffa1767227a6262f9ace305c86cd01728d
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-12-21 19:35:37 +0100

    Added key html tag

--- src/components/dropdownmenu/DropDownMenu.js
+++ src/components/dropdownmenu/DropDownMenu.js
@@ -5,7 +5,7 @@ export default function DropDownMenu({ name, id, className, data }) {
     return (
         <select name={name} id={id} className={className}>
             {data.map((elt) => (
-                <option value={elt.name}>
+                <option key={elt.name} value={elt.name}>
                     {elt.name}
                 </option>
             ))}
